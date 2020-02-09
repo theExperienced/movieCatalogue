87fa59d50c863c8f5c603f72db1d38a27ca41e78
theExperienced <mizradan@gmail.com>
2020-02-09 12:01:04 +0200

    Updates

---
+++ precache-manifest.1811bc01e266b8b02c66d9fdb883c440.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e76adeb347f8c6c13f56003f72784c6",
+    "url": "/movieCatalogue/index.html"
+  },
+  {
+    "revision": "2ac2c35705ca897427ac",
+    "url": "/movieCatalogue/static/css/main.c3ad657e.chunk.css"
+  },
+  {
+    "revision": "64beccb63a6adde9cde9",
+    "url": "/movieCatalogue/static/js/2.c059cc79.chunk.js"
+  },
+  {
+    "revision": "280b48dbc3281f342da16f66e41cf46c",
+    "url": "/movieCatalogue/static/js/2.c059cc79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ac2c35705ca897427ac",
+    "url": "/movieCatalogue/static/js/main.eadb946f.chunk.js"
+  },
+  {
+    "revision": "d76f95f38d2e789f8444",
+    "url": "/movieCatalogue/static/js/runtime-main.de368e83.js"
+  }
+]);
